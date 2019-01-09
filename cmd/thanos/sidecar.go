65e84969d2715b6387619b3596f77994072e957a
Bartek Plotka <bwplotka@gmail.com>
2019-01-09 16:03:35 +0000

    Added shipper capability to sync compacted blocks once. Hooked in a command.
    * Use shipper metrics properly.
    * Do not add compacted blocks to `uploaded` arr in thanos shipper meta, as they are not uploaded.
    
    NOTE: There is a clear race condition and lack of support for eventual consistency when uploading old blocks (created long ago).
    I will update read-write ops proposal with potential fix (using object meta data for creation time, or append creation time to meta.json).
    
    For now turning off compactor is REQUIRED. Explicitly stated that.
    
    
    Signed-off-by: Bartek Plotka <bwplotka@gmail.com>

--- cmd/thanos/sidecar.go
+++ cmd/thanos/sidecar.go
@@ -35,6 +35,8 @@ import (
 func registerSidecar(m map[string]setupFunc, app *kingpin.Application, name string) {
 	cmd := app.Command(name, "sidecar for Prometheus server")
 
+	cmd.Command("lol", "lol2")
+
 	grpcBindAddr, httpBindAddr, cert, key, clientCA, newPeerFn := regCommonServerFlags(cmd)
 
 	promURL := cmd.Flag("prometheus.url", "URL at which to reach Prometheus's API. For better performance use local network.").
